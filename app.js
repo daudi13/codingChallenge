706550340987219b62a31c6ce41cd94d41aae115
daudi13 <davouma13@gmail.com>
2021-11-05 04:33:59 +0300

    commit changes

--- app.js
+++ app.js
@@ -40,48 +40,52 @@ team2: 6.5,
 },
 };
 
-//create one player array for each team (varibales 'players' and players2)
+
+// create one player array for each team (variables 'players1' and 'players2')
 
 const [player1, player2] = game.players
 
-console.log(player1)
+console.log(player1, player2);
 
-//for Bayern Munich(team1) create one varibale ('gk') with the goalkeeper's name, and one array('fieldPlayer') with all the remaining 10 field players
+//the first player in any player array is the goalkeeper and the others are field players. For Bayern Muncih(team 1) create one variable('gk') with the goalkeeper's name and one array, and one array('fieldPlayers) with all the remaining 10 field players
 
-const [gk, ...fieldPlayer] = player1;
 
-console.log(gk);
-console.log(fieldPlayer);
+const [gk, ...fieldPlayers] = player1;
 
-//create an array 'allPlayers' containign all players pf both teams
+console.log(gk, fieldPlayers);
+
+
+//create an array 'all players' containing all players of both teams (22 players) 
 
 const allPlayers = [...player1, ...player2];
+
 console.log(allPlayers);
 
-//during the game, Bayern Munich(team 1) used 3 subtitute players. so create a new array('playersFinal') containing all the original team 1 players plus 'Thiago' 'Coutinho' and 'Perisic'
+//during the game, Bayern Muncih(team 1) used 3 subtitute aplayers. so create a new array('players1Final')containing all the original team1 layers plus 'thiago', 'coutinho' and 'perisic'
 
-game.subtitutes = ['Thiago', 'Coutinho', 'Persic'];
-const playersFinal = [...player1, ...game.subtitutes];
-console.log(playersFinal);
 
-//based on the game.odds object, create one varibale for each odd(called 'team1', 'draw', and 'team2')
+const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...player1];
 
-const {team1, x:draw , team2} = game.odds;
+console.log(players1Final)
 
-console.log(team1);
-console.log(draw);
-console.log(team2);
 
-//write a function('printGoal') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total(number of players names passed in)
+//based on the game.odds object, create one varibale for each odd(called 'team1', 'draw' and 'team2')
 
+const { team1, x: draw, team2 } = game.odds;
+
+console.log(team1, draw, team2);
+
+//write a function(printGoals) that receives an arbitrary number of players anames(not an array) and prints each of them to the consoke, along with the number of goals that were scored in total(number of player names passed in)
 
 const printGoals = function (...players) {
-    console.log(`the ${players.length} goals were scored by ${players}`)
+    console.log(`${players.length} is the number of goals that were scored by ${players}`)
 }
 
-printGoals(...game.scored);
+printGoals('Davis', 'Muller', 'Lewandowski', 'Kimmich');
 
-//the team with the lower odd is more likely to win. Print to the console which team is more likely to win, without an if?else statement or the ternary operator
+// the team with the lower odd is more likely to win. print to the console which team is more likely to win, without using an if/ else statement or the ternary operator
 
-team2 < team1 && console.log('Team 2 is more likely to win')
-team1 < team2 && console.log('Team 1 is more likely to win');
+
+team1 < team2 && console.log('team 1 is more likely to win the match');
+
+team2 < team1 && console.log('team 2 is more likely to win the match')
